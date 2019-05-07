9b63a4084ac882a4be37650d334d622e69419530
Alexis Martin <alexismartin232@gmail.com>
2019-05-07 17:21:07 +0200

    Fixed mailer

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_LOGIN'],
   :password => ENV['SENDGRID_PWD'],
-  :domain => 'monsite.fr',
+  :domain => 'https://eventbrite-v2.herokuapp.com/',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
